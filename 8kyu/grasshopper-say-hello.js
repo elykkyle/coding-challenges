823d45445e80b03eeb510bf167bf2f3d49c0c460
kyle <elykkyle@gmail.com>
2022-11-14 09:30:23 -0600

    Grasshoper - Debug sayHello

---
+++ 8kyu/grasshopper-say-hello.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+function sayHello(name) {
+  return 'Hello'
+}
+
+*/
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
