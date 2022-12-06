4133d7fd48ca1f76332ad9f07cd5401efc532a4f
kyle <15004488+elykkyle@users.noreply.github.com>
2022-12-06 11:40:51 -0600

    Beginner Series #3 Sum of (range) numbers

---
+++ 7kyu/beginner-series-3-sum-of-numbers.js
@@ -0,0 +1,46 @@
+/*
+https://www.codewars.com/kata/55f2b110f61eb01779000053
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+function getSum(a, b) {
+  const max = Math.max(a, b);
+  const min = Math.min(a, b);
+  let sum = 0;
+  if (a === b) {
+    return a;
+  }
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(getSum(0, -1));
+console.log(getSum(0, 1));
+console.log(getSum(-10, 11));
+
+/*  from Code Wars:
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
+
+function GetSum(a,b)
+{
+  return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
+
+*/
