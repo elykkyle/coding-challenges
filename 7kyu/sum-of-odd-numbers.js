f53ff3c2e9bf583d5484651cdb6d0ff1133d59b5
kyle <15004488+elykkyle@users.noreply.github.com>
2022-12-18 15:45:01 -0600

    Sum of Odd Numbers

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+
+DESCRIPTION:
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+*/
+
+const rowSumOddNumbers = (n) => n ** 3;
+
+console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(2));
+console.log(rowSumOddNumbers(3));
+console.log(rowSumOddNumbers(4));
+console.log(rowSumOddNumbers(42));
