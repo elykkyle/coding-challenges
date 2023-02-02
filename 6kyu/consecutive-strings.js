29d42303125369e3913c3d9999d32a9eb9319d63
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-02-02 09:10:22 -0600

    solved consecutive strings

--- 6kyu/consecutive-strings.js
+++ 6kyu/consecutive-strings.js
@@ -27,43 +27,31 @@ consecutive strings : follow one after another without an interruption
 
 Params: array of strings, and integer 'k' specifying how many consecutive strings to concatenate
 Returns: one string consisting of 'k' strings from array with no separator. the first longest string.
-Example: ['one', 'two', 'three', 'four'], k -> 'threefour'
-  ['kyle', 'darryl', 'joseph', 'roger'] -> 'darryljoseph
+Example: ['one', 'two', 'three', 'four'], 2 -> 'threefour'
+  ['kyle', 'darryl', 'joseph', 'roger'] 3 -> 'darryljosephroger
 Pseudocode:
   func (strarr, k){
     let longest = ''
     n = strarr.length
     if n = 0 || k > n || k <= 0
       return longest
-    for let i, i < length - 1, l++
-      if strarr[i].length + strarr[i + 1].length > longest.length
-        longest = strarr[i] + strarr[i + 1]
+    for let i, i < strarr.length && i < k, i++
+      str = strarr.slice(i, i + k),join('')
+      str.length > longest.length? longest = str : ''
     return longest
   }
-
 */
-/*
+
 function longestConsec(strarr, k) {
   let longest = '';
   const n = strarr.length;
-  if (n === 0 || k > n || k <= 0) {
-    return longest;
-  }
-  for (let i = 0; i < strarr.length - 1; i++) {
-    const strLenth
-    for (let j = k; j >= 0; j--){
-
-    }
-    if (strarr[i].length + strarr[i + 1].length > longest.length) {
-      longest = strarr[i] + strarr[i + 1];
-    }
+  if (n === 0 || k > n || k <= 0) return longest;
+  for (let i = 0; i < n - k + 1; i++) {
+    const str = strarr.slice(i, i + k).join('');
+    str.length > longest.length ? (longest = str) : '';
   }
   return longest;
 }
- */
-console.log(
-  longestConsec(['tree', 'foling', 'trashy', 'blue', 'abcdef', 'uvwxyz'], 2)
-); // folingtrashy
 
 console.log(
   longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2)
@@ -102,39 +90,3 @@ console.log(
   longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 15)
 ); // "")
 console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 0)); // "")
-
-// Solutions from codewars:
-
-function longestConsec1(strarr, k) {
-  // initialize longest variable to track the longest consecutive string
-  let longest = '';
-  // begin looping through strarr. using k and strarr.length for loop limit.
-  // k has to be greater than 0. i has to be less than the strarr.length - k
-  // (this prevents accessing non-existent array positions)
-  for (let i = 0; k > 0 && i <= strarr.length - k; i++) {
-    // create a new array from strarr with only k number of strings
-    const tempArray = strarr.slice(i, i + k);
-    // join strings in new array
-    const tempStr = tempArray.join('');
-    // compare joined string to string length of longest variable.
-    if (tempStr.length > longest.length) {
-      // if joined string is longer than longest, assign joined string to longest
-      longest = tempStr;
-    }
-  }
-  return longest;
-}
-
-function longestConsec2(strarr, k) {
-  // return if k <=0 or if k is greater than the array length (per requirements)
-  if (k <= 0 || k > strarr.length) {
-    return '';
-  }
-  // use reduce fuction, inputting 'long[est]' string as accumulator, item (unused), and index
-  return strarr.reduce((long, item, i) => {
-    //create current string using slice and join. slice starts at current index, and ends at index + k
-    const currString = strarr.slice(i, i + k).join('');
-    // compare the current string length to the long length. returns longest of the two.
-    return currString.length > long.length ? currString : long;
-  }, '');
-}
